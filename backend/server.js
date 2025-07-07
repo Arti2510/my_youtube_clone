e2016153e12ec466faf359f7090179ffa23e11e6
Arti2510 <artimaurya2510@gmail.com>
2025-07-08 01:44:55 +0530

    create server.js file

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());  // middleware for json data
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on http://localhost:${process.env.PORT}`);
+    });
+  })
+  .catch(err => console.error("MongoDB connection error:", err));
+
+
+
+
+
+
